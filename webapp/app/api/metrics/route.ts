ad1ab8c60df8d3d050df1224301ba33acfb84ded
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-15 23:08:32 +0700

    fix(webapp): specific runtime on api metric

--- webapp/app/api/metrics/route.ts
+++ webapp/app/api/metrics/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 
 import { register } from '@/lib/metrics'
 
+export const runtime = 'nodejs'
+
 export async function GET() {
   try {
     const metrics = await register.metrics()
