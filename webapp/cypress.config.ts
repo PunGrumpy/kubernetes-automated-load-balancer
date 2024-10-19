35ca7d5340f313e3c220ba43f9a547c1f87daa9b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-20 03:46:42 +0700

    chore(cypress): add project id

--- webapp/cypress.config.ts
+++ webapp/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    projectId: 'y7wyq7',
+    async setupNodeEvents(on, config) {}
   }
 })
