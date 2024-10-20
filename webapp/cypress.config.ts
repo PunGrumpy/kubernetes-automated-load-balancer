d65eb1d16b4b290b862f6b66b4a64f177cfde54b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-20 15:21:30 +0700

    fix: remove cypress

--- webapp/cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    projectId: 'y7wyq7',
-    async setupNodeEvents(on, config) {}
-  }
-})
