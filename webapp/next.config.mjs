7ab6d921958c260951c7a2938945ac722825c8e3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-24 16:13:48 +0700

    chore(env): try to add next pub

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_METADATA_BASE: process.env.NEXT_PUBLIC_METADATA_BASE,
+    NEXT_PUBLIC_AUTH_API_KEY: process.env.NEXT_PUBLIC_AUTH_API_KEY
+  }
 }
 
 export default nextConfig
